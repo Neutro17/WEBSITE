eb61e2a0ad7ee62ca25d63a3c9b6672fed5bac77
GeovanneSousa <56236554+Neutro17@users.noreply.github.com>
2020-11-10 17:39:18 -0300

    Update index.js

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,1 +1,26 @@
+import express = ('express')
+const app = ('express')
+
+app.get("/" get(request, response)){
+
+    document.write()
+}
+
+app.post("/" post(request, response)){
+    document.write()
+}
+
+app.put("/" put(request, response)){
+
+    document.write()
+}
+
+app.delete("/" delete(request, response)){
+    document.write()
+}
+
+
+
+
+
 
