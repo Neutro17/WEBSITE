5f2f908df8978b7c3d385f87981f554c1849927d
GeovanneSousa <56236554+Neutro17@users.noreply.github.com>
2020-11-12 16:39:20 -0300

    Add files via upload

---
+++ Models/index.js
@@ -0,0 +1,32 @@
+const db = require("../Controllers/db");
+
+(async () => {
+    const db = require("./db");
+    console.log('Começou!');
+ 
+    console.log('SELECT * FROM CLIENTES');
+    const clientes = await db.selectCustomers();
+    console.log(clientes);
+})();
+
+(async () => {
+    const db = require("./db");
+    console.log('Começou!');
+    
+    console.log('INSERT INTO CLIENTES');
+    const result = await db.insertCustomer({nome: "Zé", idade: 18, uf: "SP"});
+    console.log(result);
+ 
+    console.log('SELECT * FROM CLIENTES');
+    const clientes = await db.selectCustomers();
+    console.log(clientes);
+    })();
+
+    console.log('DELETE FROM CLIENTES');
+    const result3 = await db.deleteCustomer(7);
+    console.log(result3);
+
+    console.log('UPDATE CLIENTES');
+    const result2 = await db.updateCustomer(6, {nome: "Zé José", idade: 19, uf: "SP"});
+    console.log(result2);
+
